05e3fabdc443a12111fea5dbac3360687b02907e
Anthony Dmitriyev <antstorm@gmail.com>
2015-05-13 00:32:39 +0100

    Added tests for Preload initialization

--- spec/polymorphic_preloader_spec.rb
+++ spec/polymorphic_preloader_spec.rb
@@ -1,10 +1,30 @@
 require 'spec_helper'
 
 describe PolymorphicPreloader do
-  it 'can be initialized' do
-    preloader = PolymorphicPreloader.new([], :key)
+  subject { PolymorphicPreloader }
 
+  it 'can be initialized' do
+    preloader = subject.new([], :key)
 
     expect(preloader).to be_instance_of(PolymorphicPreloader)
+  end
+
+  it 'stores association key' do
+    preloader = subject.new([], :polymorphic)
+
+    expect(preloader.association_name).to be_eql(:polymorphic)
+  end
+
+  it 'groups objects by polymorphic association' do
+    object_1 = double
+    allow(object_1).to receive(:polymorphic_type) { 'First' }
+
+    object_2 = double
+    allow(object_2).to receive(:polymorphic_type) { 'Second' }
+
+    preloader = subject.new([object_1, object_2], :polymorphic)
+
+    expect(preloader.grouped_objects).to include('First' => [object_1])
+    expect(preloader.grouped_objects).to include('Second' => [object_2])
   end
 end
